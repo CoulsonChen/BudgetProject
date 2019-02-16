93eb08c6eb35b958280a52a3ad4690dcdbf9ff69
CoulsonChen <a0936792910@gmail.com>
2019-02-16 15:43:29 +0800

    First Edition

---
+++ BudegtProject_UnitTest/Accounting_UnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BudgetProject;
+
+namespace BudegtProject_UnitTest
+{
+    [TestClass]
+    public class Accounting_UnitTest
+    {
+        Accounting accounting = new Accounting();
+
+        
+
+        private void BudgetShouldBe(double Budget, DateTime StartDate, DateTime EndDate)
+        {
+            Assert.AreEqual(Budget, accounting.TotalAccoount(StartDate, EndDate));
+        }
+    }
+}
---
+++ BudegtProject_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BudegtProject_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BudegtProject_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba965116-a52a-469b-9753-7182fbab6c05")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
